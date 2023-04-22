38ad5c32e969614ea84a1d58bc6f755afc6dff77
Ihor Lev <igorlevin000@gmail.com>
2023-04-22 11:11:32 +0300

    function isMobile

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,10 +4,8 @@ const mobMenu = document.getElementById('mobileMenu');
 const popup = document.getElementById('appPopup');
 
 function isMobile() {
-    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
-        console.log(true)
-    } else {
-        console.log(false)
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent) === window.navigator.userAgent) {
+        popup.classList.remove('display-none')
     }
 }
 isMobile();
@@ -23,3 +21,4 @@ function mobileMenuOpen() {
 function mobileMenuClose() {
     mobMenu.classList.add('display-none');
 }
+
